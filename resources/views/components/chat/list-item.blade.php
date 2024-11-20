62ff7ab174a8945c5226765c28371bf8a4c005f3
brayandewitt <brayandewitt56@gmail.com>
2024-11-20 19:03:28 +0530

    cht component scroll remove

--- resources/views/components/chat/list-item.blade.php
+++ resources/views/components/chat/list-item.blade.php
@@ -1,3 +1,3 @@
-<div class="flex-1 overflow-y-scroll bg-gray-200 dark:bg-gray-700 border-0 rounded-lg p-4">
+<div class="flex-1  bg-gray-200 dark:bg-gray-700 border-0 rounded-lg p-4">
     {{ $slot }}
 </div>
