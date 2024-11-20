617b73c44b93803c52c2b90b127302839bbaa0ea
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-11-20 19:41:27 +0530

    Add search bar

--- resources/views/components/chat/list.blade.php
+++ resources/views/components/chat/list.blade.php
@@ -1,7 +1,7 @@
 <aside id="logo-sidebar" class="h-full w-1/4 space-y-4 overflow-y-auto text-white transition-transform sm:translate-x-0"
     aria-label="Sidebar">
     <div class="p-4">
-        <h2 class="text-xl font-semibold">Chats</h2>
+        <x-search-bar></x-search-bar>
     </div>
     <div class="flex flex-col space-y-4 overflow-y-auto px-3">
         <ul class="space-y-2 font-medium">
