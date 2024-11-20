e85b65757f941ed40ab0549a6b1be681c5268313
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-11-20 19:41:45 +0530

    Changed some styles

--- resources/views/components/chat/canvas.blade.php
+++ resources/views/components/chat/canvas.blade.php
@@ -1,3 +1,3 @@
-<div class="align-center relative h-full w-full flex-col justify-end pt-1">
+<div class="align-center relative h-full w-full flex-col justify-end pt-1 space-y-4">
     {{$slot}}
 </div>
