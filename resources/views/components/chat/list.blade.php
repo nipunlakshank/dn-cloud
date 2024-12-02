6c35c4b14689116d0bd26b8f9dafb10af25fed17
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-12-02 16:17:52 +0530

    make search bar and options icon stick to top when scrolling

--- resources/views/components/chat/list.blade.php
+++ resources/views/components/chat/list.blade.php
@@ -1,6 +1,6 @@
-<aside id="logo-sidebar" class="h-full pb-4 w-1/4 space-y-4 overflow-y-auto text-white transition-transform hidden lg:block"
+<aside id="logo-sidebar" class="relative h-full pb-4 w-1/4 space-y-4 overflow-y-auto text-white transition-transform hidden lg:block"
     aria-label="Sidebar">
-    <div class="p-4">
+    <div class="p-4 sticky top-0 bg-gray-100 dark:bg-gray-900">
         <x-chat.dropdown></x-chat.dropdown>
         <x-search-bar></x-search-bar>
     </div>
