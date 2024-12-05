1390f453e0ff4d3e318f8aec7c883d968ab9eeb0
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-12-05 16:09:45 +0530

    increase top padding in sidebar

--- resources/views/components/chat/list.blade.php
+++ resources/views/components/chat/list.blade.php
@@ -1,7 +1,7 @@
 <aside id="chat-sidebar"
     class="relative hidden h-full w-full space-y-4 overflow-y-auto pb-4 text-white transition-transform lg:block lg:w-1/4"
     aria-label="Sidebar">
-    <div class="sticky top-0 flex justify-between gap-2 bg-gray-100 p-4 dark:bg-gray-900">
+    <div class="sticky top-0 flex justify-between gap-2 bg-gray-100 p-4 pt-6 dark:bg-gray-900">
         <x-chat.dropdown></x-chat.dropdown>
         <x-search-bar></x-search-bar>
     </div>
