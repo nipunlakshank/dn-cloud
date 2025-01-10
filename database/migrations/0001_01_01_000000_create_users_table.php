83445fa351e8a2024378eadcf35062be70feb8b9
Nipun Lakshan <nipunlakshankumara@gmail.com>
2025-01-10 20:55:36 +0530

    change(db): add new column(is_active) to users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('role', ['admin', 'supervisor', 'accountant', 'worker'])->default('worker');
+            $table->boolean('is_active')->default(true);
             $table->rememberToken();
             $table->timestamps();
         });
