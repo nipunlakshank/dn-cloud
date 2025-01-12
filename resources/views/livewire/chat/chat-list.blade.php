5cd6ffcc1c084e7467788a2e37df8a6b519083c6
Nipun Lakshan <nipunlakshankumara@gmail.com>
2025-01-12 14:20:16 +0530

    fix(ui): chat list item styles

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -5,21 +5,19 @@
         <x-chat.dropdown></x-chat.dropdown>
         <x-search-bar></x-search-bar>
     </div>
-    <div class="flex flex-col space-y-4 overflow-y-auto px-3">
-        <ul class="space-y-2 font-medium">
-            {{-- Chat list items --}}
-            <x-chat.list-item>
-                <x-slot:chatName>John Doe</x-slot:chatName>
-                <x-slot:lastMessage>Message 1 Lorem ipsum dolor sit amet.</x-slot:lastMessage>
-                <x-slot:time>11:46</x-slot:time>
-            </x-chat.list-item>
+    <div class="flex flex-col space-y-4 overflow-y-auto px-3 py-1">
+        {{-- Chat list items --}}
+        <x-chat.list-item>
+            <x-slot:chatName>John Doe</x-slot:chatName>
+            <x-slot:lastMessage>Message 1 Lorem ipsum dolor sit amet.</x-slot:lastMessage>
+            <x-slot:time>11:46</x-slot:time>
+        </x-chat.list-item>
 
-            <!-- Additional chat list items -->
-            @foreach (range(2, 14) as $i)
-                <x-chat.list-item>
-                    <x-slot:lastMessage>Message {{ $i }} Lorem ipsum dolor sit amet.</x-slot:lastMessage>
-                </x-chat.list-item>
-            @endforeach
-        </ul>
+        <!-- Additional chat list items -->
+        @foreach (range(2, 14) as $i)
+            <x-chat.list-item>
+                <x-slot:lastMessage>Message {{ $i }} Lorem ipsum dolor sit amet.</x-slot:lastMessage>
+            </x-chat.list-item>
+        @endforeach
     </div>
 </aside>
