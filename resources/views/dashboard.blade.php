e34323133206e1466f78c46f8056780cc17998f9
BrayanDewitt <brayan@Brayan.>
2025-01-28 22:27:23 +0530

    ui update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -100,7 +100,7 @@
     <!-- Main Content Area -->
     <div class="flex h-full w-full flex-col">
 
-        <!-- dashbord Content Scrollable Area -->
+        <!-- dashbord Content Scrollable Arear-->
         <div class="flex flex-col gap-10 overflow-y-scroll p-4">
             <div class="flex flex-col gap-3 lg:flex-row">
                 <a href="/wallets"
