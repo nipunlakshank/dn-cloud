df28006b1f2720dec04a82c3df5314e7bcbf06bc
BrayanDewitt <brayan@Brayan.>
2025-01-28 22:43:23 +0530

    file name changes

--- resources/views/components/settings/navigation.blade.php
+++ resources/views/components/settings/navigation.blade.php
@@ -1,5 +1,10 @@
-<!-- Settings Modal -->
-<div id="settings-modal" tabindex="-1" aria-hidden="true"
+{{-- <button type="button" data-modal-target="crypto-modal" data-modal-toggle="crypto-modal" class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700">
+<svg aria-hidden="true" class="w-4 h-4 me-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"></path></svg>
+Connect wallet
+</button> --}}
+
+<!-- Main modal -->
+<div id="crypto-modal" tabindex="-1" aria-hidden="true"
     class="fixed left-0 right-0 top-0 z-50 hidden h-[calc(100%-1rem)] max-h-full w-full items-center justify-center overflow-y-auto overflow-x-hidden md:inset-0">
     <div class="relative max-h-full w-full max-w-md p-4">
         <!-- Modal content -->
@@ -11,13 +16,13 @@
                 </h3>
                 <button type="button"
                     class="ms-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white"
-                    data-modal-toggle="settings-modal">
+                    data-modal-toggle="crypto-modal">
                     <svg class="h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                         viewBox="0 0 14 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                     </svg>
-                    <span class="sr-only">Close</span>
+                    <span class="sr-only">Close modal</span>
                 </button>
             </div>
             <!-- Modal body -->
@@ -25,6 +30,7 @@
                 <p class="text-sm font-normal text-gray-500 dark:text-gray-400">Connect with one of our available wallet
                     providers or create a new one.</p>
                 <ul class="my-4 space-y-3">
+
                     <li>
                         <button type="button"
                             x-data="{ darkMode: localStorage.getItem('theme') === 'dark' }"
@@ -32,12 +38,22 @@
                             class="group flex w-full items-center rounded-lg bg-gray-50 p-3 text-start text-base font-bold text-gray-900 hover:bg-gray-100 hover:shadow dark:bg-gray-600 dark:text-white dark:hover:bg-gray-500">
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                                 xmlns="http://www.w3.org/2000/svg">
-                                <path d="M12 21C7.02944 21 3 16.9706 3 12C3 7.19723 6.76201 3.27311 11.5 3.01367V3C11.1456 3.96621 11 4.91097 11 6.00002C11 10.9706 15.0294 15 20 15C20.2387 15 20.2539 15.0183 20.4879 15C19.2524 18.4956 15.9187 21 12 21Z" stroke="#111928" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                <path
+                                    d="M12 21C7.02944 21 3 16.9706 3 12C3 7.19723 6.76201 3.27311 11.5 3.01367V3C11.1456 3.96621 11 4.91097 11 6.00002C11 10.9706 15.0294 15 20 15C20.2387 15 20.2539 15.0183 20.4879 15C19.2524 18.4956 15.9187 21 12 21Z"
+                                    stroke="#111928" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
+
                             <span class="ms-3 flex-1 whitespace-nowrap">Dark Mode</span>
+
                             <label class="inline-flex cursor-pointer items-center lg:hidden">
-                                <input type="checkbox" x-on:click="darkMode = !darkMode" x-model="darkMode" class="peer sr-only">
-                                <div class="peer relative h-5 w-9 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-4 after:w-4 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"></div>
+                                <input type="checkbox"
+                                    x-on:click="darkMode = !darkMode"
+                                    x-model="darkMode"
+                                    class="peer sr-only">
+                                <div
+                                    class="peer relative h-5 w-9 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-4 after:w-4 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800">
+                                </div>
+
                             </label>
 
                             <label class="hidden cursor-pointer items-center lg:inline-flex">
@@ -45,8 +61,10 @@
                                     x-on:click="darkMode = !darkMode"
                                     x-model="darkMode"
                                     class="peer sr-only">
-                                <div class="peer relative h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800">
+                                <div
+                                    class="peer relative h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800">
                                 </div>
+
                             </label>
                         </button>
                     </li>
@@ -62,13 +80,17 @@
                             <span class="ms-3 flex-1 whitespace-nowrap">Log Out</span>
                         </a>
                     </li>
+
                 </ul>
+
             </div>
         </div>
     </div>
+
 </div>
 <x-settings.registration></x-settings.registration>
 <x-settings.profile></x-settings.profile>
-<x-settings.addGroup></x-settings.addGroup>
-<x-settings.newChat></x-settings.newChat>
+<x-settings.add-group></x-settings.add-group>
+<x-settings.new-chat></x-settings.new-chat>
 <x-settings.logout></x-settings.logout>
+
