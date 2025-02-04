bb4c1688629f5f90678bd28924ed645a53fdbc45
Nipun Lakshan <nipunlakshankumara@gmail.com>
2025-02-04 19:40:46 +0530

    enhance(ui): make border more visible in light theme

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -1,5 +1,5 @@
 <aside aria-label="Sidebar"
-    class="h-full border border-transparent border-r-gray-200 dark:border-r-gray-700">
+    class="h-full border border-transparent border-r-gray-300 dark:border-r-gray-700">
     <div class="sticky top-0 z-10 flex justify-between gap-2 bg-gray-100 p-4 pt-6 dark:bg-gray-900">
         <x-chat.dropdown></x-chat.dropdown>
         <x-search-bar></x-search-bar>
