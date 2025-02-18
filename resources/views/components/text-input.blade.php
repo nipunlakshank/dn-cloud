3d4d2b25a6d4711e8cf5f10e8b59450026ea5ca1
Madhukawish <68007042+Madhukawish@users.noreply.github.com>
2025-02-19 00:08:54 +0530

    fix : Improve login features
    - Add "Inactive" user checking & redirect to notice
    - Add email "Verified" check & redirect to verify email
    - Add Inactive Notice page
    - Add route in web.php to "Inactive Notice"
    - Text-Input updated

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,4 +1,4 @@
-@props(['disabled' => false])
+@props(['disabled' => false,'autofocus'=> false])
 
-<input @disabled($disabled)
+<input @disabled($disabled) {{ $autofocus ? "autofocus" : ''}}
     {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) }}>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
 
+Route::get('inactive-notice', function () {
+    return View('auth.inactive-notice');
+})->name('notice.inactive');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
