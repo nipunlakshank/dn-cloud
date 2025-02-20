a45e5d7873866d4be8a6cfa85cced51e668a6950
Nipun Lakshan <nipunlakshankumara@gmail.com>
2025-02-20 21:46:41 +0530

    feat: send documents in chat

--- app/Livewire/Modals/DocumentAttachmentModal.php
+++ app/Livewire/Modals/DocumentAttachmentModal.php
@@ -2,10 +2,48 @@
 
 namespace App\Livewire\Modals;
 
+use App\Models\Chat;
+use App\Services\MessageService;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\On;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class DocumentAttachmentModal extends Component
 {
+    use WithFileUploads;
+
+    public Chat $chat;
+    public array $documents;
+    public string $text;
+
+    public function send()
+    {
+        $message = app(MessageService::class)->send(
+            $this->chat,
+            Auth::user(),
+            $this->text,
+            'document',
+            $this->documents,
+        );
+
+        if ($message) {
+            $this->reset(['documents', 'text']);
+        }
+    }
+
+    #[On('chat.select')]
+    public function selectChat(Chat $chat)
+    {
+        $this->chat = $chat;
+    }
+
+    public function mount()
+    {
+        $this->documents = [];
+        $this->text = '';
+    }
+
     public function render()
     {
         return view('livewire.modals.document-attachment-modal');
