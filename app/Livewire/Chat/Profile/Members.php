8ed474759750f4202f4caf1db3518429a70c3a89
Nipun Lakshan <nipunlakshankumara@gmail.com>
2025-03-03 21:46:13 +0530

    Revert "Fix : Chat profile add user to group"
    This reverts commit 2c2ee2378be59208b32b93560cb47207366a96c7.

--- app/Livewire/Chat/Profile/Members.php
+++ app/Livewire/Chat/Profile/Members.php
@@ -4,39 +4,33 @@ namespace App\Livewire\Chat\Profile;
 
 use App\Models\Chat;
 use App\Models\ChatUser;
-use Livewire\Attributes\On;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class Members extends Component
 {
     public Chat $chat;
-    public $chatMembers = '';
 
-    public function mount($chat)
+    public $chatMembers = '';
+    public $allUsers = [];
+
+    public function mount($chat, $users)
     {
         $this->chat = $chat;
         $this->chatMembers = $chat->users;
+        $this->allUsers = $users;
     }
-
     public function render()
     {
         return view('livewire.chat.profile.members');
     }
 
+    public function getChatMembers() {}
+
     public function removeMember($user_id, $chat_id)
     {
         ChatUser::where('user_id', '=', $user_id, 'and')->where('chat_id', '=', $chat_id)->delete();
         $this->chatMembers = $this->chat->users;
-        $this->dispatch('member.updated');
-    }
-
-    #[On('group-addMember')]
-    public function addMemberToGroup($params)
-    {
-        ChatUser::create(['chat_id' => $this->chat->id, 'user_id' => $params['user_id'], 'role' => 'member']);
-        $this->chat = Chat::firstWhere('id', $this->chat->id);
-        $this->chatMembers = $this->chat->users;
-        $this->dispatch('member.updated');
-        $this->dispatch('member.list.update')->to(SelectUserDropdown::class);
+        $this->dispatch('removed');
     }
 }
--- app/Livewire/Chat/Profile/ProfileInfo.php
+++ app/Livewire/Chat/Profile/ProfileInfo.php
@@ -2,21 +2,26 @@
 
 namespace App\Livewire\Chat\Profile;
 
+use App\Models\User;
 use Livewire\Component;
 
 class ProfileInfo extends Component
 {
     public $chat;
+
     public $isGroup = false;
+
+    public $users;
 
     public function mount($chat)
     {
         $this->chat = $chat;
         $this->isGroup = $this->chat->is_group;
+        $this->users = User::all()->toArray();
     }
 
     public function render()
     {
-        return view('livewire.chat.profile.profile-info');
+        return view('livewire.chat.profile.profile-info', ['users' => $this->users]);
     }
 }
