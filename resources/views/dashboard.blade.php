7cd3de5fdf2d5600ddaac74c9a335fd046de2ccb
Nipun Lakshan <nipunlakshankumara@gmail.com>
2025-03-03 22:05:13 +0530

    fix(ui): text invisible in light mode

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
             <ul class="space-y-2 font-medium">
                 <li class="group">
                     <a href="/dashboard"
-                        class="group flex items-center rounded-lg p-2 text-gray-900 text-inherit hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                        class="group flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
                         <svg class="font-in h-5 w-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
                             viewBox="0 0 22 21">
