ebe5407234eb79808a5d39afa910ef2b997d9695
Nipun Lakshan <nipunlakshankumara@gmail.com>
2025-03-03 17:16:11 +0530

    refactor: remove unused things in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,6 @@
 import defaultTheme from "tailwindcss/defaultTheme";
 import forms from "@tailwindcss/forms";
 
-function withOpacity(variableName) {
-    return ({ opacityValue }) => {
-        if (opacityValue !== undefined) {
-            return `rgba(var(${variableName}), ${opacityValue})`;
-        }
-
-        return `rgb(var(${variableName}))`;
-    };
-}
-
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
@@ -25,30 +15,6 @@ export default {
         extend: {
             fontFamily: {
                 sans: ["Figtree", ...defaultTheme.fontFamily.sans],
-            },
-
-            backgroundColor: {
-                primary: withOpacity("--color-primary"),
-                seconday: withOpacity("--color-secondary"),
-                accent1: withOpacity("--color-accent-1"),
-                accent2: withOpacity("--color-accent-2"),
-                hover1: withOpacity("--btn-hover-color1"),
-                hover2: withOpacity("--btn-hover-color2"),
-                hover3: withOpacity("--btn-hover-color3"),
-                fill: withOpacity("--background-fill"),
-                font: withOpacity("--font-color"),
-                header: withOpacity("--font-color1"),
-            },
-
-            textColor: {
-                primary: withOpacity("--color-primary"),
-                seconday: withOpacity("--color-secondary"),
-                accent1: withOpacity("--color-accent-1"),
-                accent2: withOpacity("--color-accent-2"),
-                hover: withOpacity("--color-hover"),
-                fill: withOpacity("--color-fill"),
-                font: withOpacity("--font-color"),
-                header: withOpacity("--font-color1"),
             },
         },
     },
