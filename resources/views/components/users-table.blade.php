c34825e406a13496b82a2565d1bb29a50d7f9234
Nipun Lakshan <nipunlakshankumara@gmail.com>
2025-03-05 01:05:33 +0530

    chore: remove user id from users table in dashboard

--- resources/views/components/users-table.blade.php
+++ resources/views/components/users-table.blade.php
@@ -3,9 +3,6 @@
         <thead class="sticky top-0 z-20 bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
             <tr>
                 <th scope="col" class="px-6 py-3 max-[990px]:px-3 max-sm:px-2">
-                    Id
-                </th>
-                <th scope="col" class="px-6 py-3 max-[990px]:px-3 max-sm:px-2">
                     Users
                 </th>
                 <th scope="col" class="px-6 py-3 max-[990px]:px-3 max-sm:px-2">
@@ -17,10 +14,7 @@
             @foreach ($users as $user)
                 <tr
                     class="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600">
-                    <td class="px-6 py-4 max-[990px]:px-3 max-sm:px-2">
-                        {{ $user->id }}
-                    </td>
-                    <th scope="row"
+                    <td scope="row"
                         class="flex flex-col whitespace-nowrap px-6 py-4 font-medium text-gray-900 max-[990px]:px-3 max-sm:px-2 dark:text-white">
                         <span class="flex items-center gap-2">
                             <span>{{ $user->first_name . ' ' . $user->last_name }}</span>
@@ -32,7 +26,7 @@
                         <span class="text-xs">
                             {{ $user->email }}
                         </span>
-                    </th>
+                    </td>
                     <td class="whitespace-nowrap px-6 py-4 text-gray-900 max-[990px]:px-3 max-sm:px-2 dark:text-white">
                         @can('changeRole', $user)
                             <form action="{{ route('user-status') }}"
