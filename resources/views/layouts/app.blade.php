a0aea59741d09ed6d730837bb0608f5efe0207f0
Nipun Lakshan <nipunlakshankumara@gmail.com>
2025-03-13 23:48:18 +0530

    refactor: update page title in layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,14 +6,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'DN Cloud') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" /> -->
-        <!-- <script defer src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script> -->
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,17 +4,16 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Online Chatting Landing Page</title>
+        <title>{{ config('app.name', 'DN Cloud') }}</title>
+
         <!-- Styles / Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
         <script defer type="text/javascript">
             const ROOT = "{{ URL::to('/') }}"
         </script>
-        <script defer src="js/functions.js"></script>
-        <script defer src="js/script.js"></script>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
 
-    <body class="flex min-h-screen flex-col justify-between bg-fill font-sans transition-colors dark:bg-gray-800">
+    <body class="bg-fill flex min-h-screen flex-col justify-between font-sans transition-colors dark:bg-gray-800">
         <div
             class="container mx-auto flex h-full flex-grow flex-col sm:max-w-full md:max-w-full lg:max-w-full xl:max-w-full 2xl:max-w-full">
             <!-- Navbar -->
@@ -23,7 +22,7 @@
             </x-nav>
 
             <!-- Hero Section -->
-            <section class="flex flex-col items-center justify-center px-6 py-10 xl:flex-row md:px-8 md:py-20">
+            <section class="flex flex-col items-center justify-center px-6 py-10 md:px-8 md:py-20 xl:flex-row">
                 <!-- Illustration -->
                 <div class="align-center mb-8 flex flex-1 justify-center md:mb-0">
                     <img src="img/homebackground.png" alt="">
@@ -31,11 +30,11 @@
 
                 <!-- Text Content -->
                 <div class="flex-2 text-center md:text-left">
-                    <h1 class="text-6xxl mb-3 font-bold leading-tight md:text-6xl text-header">Have your
+                    <h1 class="text-6xxl text-header mb-3 font-bold leading-tight md:text-6xl">Have your
                         <br> best
                         chat
                     </h1>
-                    <p class="mb-6 text-sm text-font md:text-base ">
+                    <p class="text-font mb-6 text-sm md:text-base">
 
                         We are Online chat application for companys to share them files.
                     </p>
