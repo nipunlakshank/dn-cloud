f3987f9233f52681542464b2faf73e1aaa373e3c
Nipun Lakshan <nipunlakshankumara@gmail.com>
2025-03-13 20:52:57 +0530

    fix(ui): inconsistent styles in login page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'DN Cloud') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -18,7 +18,7 @@
 
     <body class="font-sans text-gray-900 antialiased">
         <div
-            class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900">
+            class="flex min-h-screen flex-col items-center justify-center bg-gray-100 pt-6 sm:pt-0 dark:bg-gray-900">
             <div>
                 <a href="/">
                     <x-application-logo class="h-20 w-20 fill-current text-gray-500" />
@@ -26,7 +26,7 @@
             </div>
 
             <div
-                class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg dark:bg-gray-800">
+                class="mt-6 w-full max-w-md overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-lg sm:rounded-lg dark:bg-gray-800">
                 {{ $slot }}
             </div>
         </div>
