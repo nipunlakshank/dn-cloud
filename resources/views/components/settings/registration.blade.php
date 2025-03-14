5b2ac677322bca3fb7ae3250b161e20721ec95bb
Madhukawish <68007042+Madhukawish@users.noreply.github.com>
2025-03-14 18:55:36 +0530

    Fix : Registration Modal Initializing Error

--- resources/views/components/settings/registration.blade.php
+++ resources/views/components/settings/registration.blade.php
@@ -1,5 +1,23 @@
 <!-- User Registration modal -->
-<div id="user-registration-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+<div
+    x-data="{
+        target:document.getElementById('user-registration-modal'),
+        options:{
+            placement: 'center-center',
+            backdrop: 'dynamic',
+            backdropClasses: 'bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-40',
+            closable: true,
+        },
+        instanceOptions:{
+            id: 'user-registration-modal',
+            override: true
+        },
+    }"
+    x-init="userRegistrationModal = new Modal(target,options,instanceOptions)"
+    x-on:open-registration-modal.window="userRegistrationModal.show()"
+    id="user-registration-modal"
+    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+
     <div class="relative p-4 w-full max-w-md max-h-full">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -8,7 +26,10 @@
                 <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
                     Registration
                 </h3>
-                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="user-registration-modal">
+                <button
+                    type="button"
+                    x-on:click="userRegistrationModal.hide()"
+                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                     </svg>
@@ -34,7 +55,7 @@
                 </div>
 
                 <input type="text" id="role" name="role" value="worker" hidden />
-                <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <button x-on:click="userRegistrationModal.hide()" type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Register
                 </button>
             </form>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -71,8 +71,9 @@
                 @can('create', App\Models\User::class)
                 <li class="group">
                     <button
+                        x-data="{userRegistrationModal:null}"
                         type="button"
-                        @click="$dispatch('open-registration-modal')"
+                        x-on:click="$dispatch('open-registration-modal')"
                         class="group flex w-full items-center rounded-lg p-2 text-start text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
                         <svg class="h-5 w-5 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
